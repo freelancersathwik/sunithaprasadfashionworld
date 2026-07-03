f15e3f420acdea5fe448817f5571eccf0b71ceb0
freelancersathwik <s78511535@gmail.com>
2026-07-03 20:05:32 +0530

    Add logging to show ImageKit configuration values

--- server/src/utils/imagekit.js
+++ server/src/utils/imagekit.js
@@ -3,6 +3,11 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+console.log('ImageKit Configuration:');
+console.log('URL Endpoint:', process.env.IMAGEKIT_URL_ENDPOINT);
+console.log('Public Key:', process.env.IMAGEKIT_PUBLIC_KEY ? 'set' : 'not set');
+console.log('Private Key:', process.env.IMAGEKIT_PRIVATE_KEY ? 'set' : 'not set');
+
 const imagekit = new ImageKit({
   urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
   publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
